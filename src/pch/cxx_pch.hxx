8cb09e440774b79c15d72d84453ab555eafbfb4d
thermitegod <thermitethegod@gmail.com>
2021-12-07 09:33:52 -0700

    pch for c and c++

---
+++ src/pch/cxx_pch.hxx
@@ -0,0 +1,28 @@
+/*
+ *
+ * Copyright: See COPYING file that comes with this distribution
+ *
+ */
+
+#pragma once
+
+// SYSTEM
+#include <string>
+#include <vector>
+#include <memory>
+#include <iostream>
+#include <cstdint>
+#include <fstream>
+#include <filesystem>
+
+// GTK
+#include <gtk/gtk.h>
+
+// GDK
+#include <gdk/gdk.h>
+#include <gdk/gdkkeysyms.h>
+
+// GLIB
+#include <glib.h>
+#include <glib/gi18n.h>
+#include <glib-object.h>
