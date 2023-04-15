5aadb642c17efd9ef55438738c432b32885a1b48
thermitegod <thermitethegod@gmail.com>
2023-04-15 13:06:02 -0700

    misc meson

--- conf.sh
+++ conf.sh
@@ -1,13 +1,9 @@
-#!/bin/bash
+#!/usr/bin/fish
 
-mkdir -p ./build
-cd ./build
+#CC=gcc CXX=g++ meson setup --buildtype=debug --prefix=$(pwd)/build ./build
+#CC=gcc CXX=g++ meson setup --buildtype=release --prefix=$(pwd)/build ./build
 
-#CC=gcc CXX=g++ meson setup --buildtype=debug ..
-#CC=gcc CXX=g++ meson setup --buildtype=release ..
+CC=clang CXX=clang++ meson setup --buildtype=debug --prefix=$(pwd)/build ./build
+#CC=clang CXX=clang++ meson setup --buildtype=release --prefix=$(pwd)/build ./build
 
-CC=/usr/lib/llvm/16/bin/clang CXX=/usr/lib/llvm/16/bin/clang++ meson setup --buildtype=debug ..
-#CC=/usr/lib/llvm/16/bin/clang CXX=/usr/lib/llvm/16/bin/clang++ meson setup --buildtype=release ..
-
-#CC=clang CXX=clang++ CC_LD=mold CXX_LD=mold meson setup --buildtype=debug ..
-#CC=clang CXX=clang++ CC_LD=mold CXX_LD=mold meson setup --buildtype=release ..
+#CC=clang CXX=clang++ meson setup -Db_sanitize=address,undefined -Db_lundef=false --buildtype=debug --prefix=$(pwd)/build ./build
