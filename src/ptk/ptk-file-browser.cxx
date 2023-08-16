c89e3532d28ed975539b709dec904cbb6915bcfe
thermitegod <43489010+thermitegod@users.noreply.github.com>
2023-08-15 17:44:28 -0700

    remove pango version check
    pango 1.44 was released in 2019, almost 4 years ago, it is safe to
    now remove the version check.

--- src/ptk/ptk-file-browser.cxx
+++ src/ptk/ptk-file-browser.cxx
@@ -1986,11 +1986,8 @@ create_folder_view(PtkFileBrowser* file_browser, ptk::file_browser::view_mode vi
     const i32 big_icon_size = vfs_mime_type_get_icon_size_big();
     const i32 small_icon_size = vfs_mime_type_get_icon_size_small();
 
-    PangoAttrList* attr_list;
-    attr_list = pango_attr_list_new();
-#if PANGO_VERSION_CHECK(1, 44, 0)
+    PangoAttrList* attr_list = pango_attr_list_new();
     pango_attr_list_insert(attr_list, pango_attr_insert_hyphens_new(false));
-#endif
 
     switch (view_mode)
     {
