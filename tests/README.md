b28d092bc2ae2c4f2d221f71c305ecc03073a92d
thermitegod <43489010+thermitegod@users.noreply.github.com>
2023-11-02 21:00:45 -0700

    inital empty test suite

---
+++ tests/README.md
@@ -0,0 +1,9 @@
+# Build And Run Tests
+
+```sh
+$ mkdir build
+$ cd build
+$ meson setup ..
+$ ninja
+$ ./test_suite
+```
---
+++ tests/src/main.cxx
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C) 2023 Brandon Zorn <brandonzorn@cock.li>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <locale>
+
+#include <gtest/gtest.h>
+
+int
+main(int argc, char** argv)
+{
+    std::locale::global(std::locale(""));
+
+    testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
