thermitegod/spacefm

commit 7913d4d40af1e6b1f02bdf719f4644d838071f52

gtk4: convert GdkEvent
convert GdkEventKey and GdkEventButton into GdkEvent.

add multiple gtk4 api wrappers for GdkEvent to work with the gtk3 api.

remove direct access to the GdkEvent struct and must now use accessors
for gtk4 compat.

src/ptk/ptk-file-task.cxx
@@ -2057,10 +2057,11 @@ namespace ptk::file_task
 }
 
 static bool
-on_query_input_keypress(GtkWidget* widget, GdkEventKey* event, PtkFileTask* ptask)
+on_query_input_keypress(GtkWidget* widget, GdkEvent* event, PtkFileTask* ptask)
 {
     (void)ptask;
-    if (event->keyval == GDK_KEY_Return || event->keyval == GDK_KEY_KP_Enter)
+    const auto keyval = gdk_key_event_get_keyval(event);
+    if (keyval == GDK_KEY_Return || keyval == GDK_KEY_KP_Enter)
     {
         // User pressed enter in rename/overwrite dialog
         const auto new_name = multi_input_get_text(widget);

src/compat/gtk4-porting.cxx
@@ -67,3 +67,42 @@ gtk4_dialog_run(GtkDialog* dialog)
 }
 
 #endif
+
+#if (GTK_MAJOR_VERSION == 3)
+
+guint
+gdk_key_event_get_keyval(GdkEvent* event)
+{
+    guint keyval;
+    gdk_event_get_keyval(event, &keyval);
+    return keyval;
+}
+
+GdkModifierType
+gdk_event_get_modifier_state(GdkEvent* event)
+{
+    GdkModifierType state;
+    gdk_event_get_state(event, &state);
+    return state;
+}
+
+guint
+gdk_button_event_get_button(GdkEvent* event)
+{
+    guint button;
+    gdk_event_get_button(event, &button);
+    return button;
+}
+
+gboolean
+gdk_event_get_position(GdkEvent* event, double* x, double* y)
+{
+    double xx;
+    double yy;
+    const auto ret = gdk_event_get_coords(event, &xx, &yy);
+    *x = xx;
+    *y = yy;
+    return ret;
+}
+
+#endif

src/compat/gtk4-porting.hxx
@@ -34,3 +34,13 @@ i32 gtk4_dialog_run(GtkDialog* dialog);
 #define gtk_widget_destroy(widget) ((void)(widget))
 
 #endif
+
+#if (GTK_MAJOR_VERSION == 3)
+
+// Fake gtk4 compat apis go here
+guint gdk_key_event_get_keyval(GdkEvent* event);
+GdkModifierType gdk_event_get_modifier_state(GdkEvent* event);
+guint gdk_button_event_get_button(GdkEvent* event);
+gboolean gdk_event_get_position(GdkEvent* event, double* x, double* y);
+
+#endif

src/ipc-command.cxx
@@ -1929,6 +1929,7 @@ run_ipc_command(const std::string_view socket_commands_json)
     }
     else if (ztd::same(command, "emit-key"))
     { // KEYCODE [KEYMOD]
+#if 0
         const std::vector<std::string> data = json["data"];
 
         // this only handles keys assigned to menu items
@@ -1946,6 +1947,9 @@ run_ipc_command(const std::string_view socket_commands_json)
             return {SOCKET_INVALID, std::format("invalid keycode '{}'", data[i])};
         }
         gdk_event_free((GdkEvent*)event);
+#else
+        return {SOCKET_INVALID, "Not Implemented"};
+#endif
     }
     else if (ztd::same(command, "activate"))
     {

src/main-window.cxx
@@ -88,10 +88,10 @@ static void on_file_browser_panel_change(PtkFileBrowser* file_browser, MainWindo
 static bool on_tab_drag_motion(GtkWidget* widget, GdkDragContext* drag_context, i32 x, i32 y,
                                u32 time, PtkFileBrowser* file_browser);
 
-static bool on_main_window_keypress(MainWindow* main_window, GdkEventKey* event, xset_t known_set);
+static bool on_main_window_keypress(MainWindow* main_window, GdkEvent* event, xset_t known_set);
 static bool on_main_window_keypress_found_key(MainWindow* main_window, xset_t set);
-static bool on_window_button_press_event(GtkWidget* widget, GdkEventButton* event,
-                                         MainWindow* main_window); // sfm
+static bool on_window_button_press_event(GtkWidget* widget, GdkEvent* event,
+                                         MainWindow* main_window);
 static void on_new_window_activate(GtkMenuItem* menuitem, void* user_data);
 static void main_window_close(MainWindow* main_window);
 
@@ -941,7 +941,7 @@ on_menu_bar_event(GtkWidget* widget, GdkEvent* event, MainWindow* main_window)
 }
 
 static bool
-bookmark_menu_keypress(GtkWidget* widget, GdkEventKey* event, void* user_data)
+bookmark_menu_keypress(GtkWidget* widget, GdkEvent* event, void* user_data)
 {
     (void)event;
     (void)user_data;
@@ -1810,22 +1810,24 @@ main_window_get_counts(PtkFileBrowser* file_browser)
 }
 
 static bool
-notebook_clicked(GtkWidget* widget, GdkEventButton* event,
-                 PtkFileBrowser* file_browser) // MOD added
+notebook_clicked(GtkWidget* widget, GdkEvent* event, PtkFileBrowser* file_browser)
 {
     (void)widget;
     MainWindow* main_window = file_browser->main_window();
     on_file_browser_panel_change(file_browser, main_window);
 
+    const auto button = gdk_button_event_get_button(event);
+    const auto type = gdk_event_get_event_type(event);
+
     // middle-click on tab closes
-    if (event->type == GdkEventType::GDK_BUTTON_PRESS)
+    if (type == GdkEventType::GDK_BUTTON_PRESS)
     {
-        if (event->button == 2)
+        if (button == 2)
         {
             file_browser->close_tab();
             return true;
         }
-        else if (event->button == 3)
+        else if (button == 3)
         {
             GtkWidget* popup = gtk_menu_new();
 
@@ -2670,24 +2672,26 @@ on_tab_drag_motion(GtkWidget* widget, GdkDragContext* drag_context, i32 x, i32 y
 }
 
 static bool
-on_window_button_press_event(GtkWidget* widget, GdkEventButton* event,
-                             MainWindow* main_window) // sfm
+on_window_button_press_event(GtkWidget* widget, GdkEvent* event, MainWindow* main_window)
 {
     (void)widget;
-    if (event->type != GdkEventType::GDK_BUTTON_PRESS)
+    const auto type = gdk_event_get_event_type(event);
+    if (type != GdkEventType::GDK_BUTTON_PRESS)
     {
         return false;
     }
 
+    const auto button = gdk_button_event_get_button(event);
+
     // handle mouse back/forward buttons anywhere in the main window
-    if (event->button == 4 || event->button == 5 || event->button == 8 || event->button == 9) // sfm
+    if (button == 4 || button == 5 || button == 8 || button == 9)
     {
         PtkFileBrowser* file_browser = main_window->current_file_browser();
         if (!file_browser)
         {
             return false;
         }
-        if (event->button == 4 || event->button == 8)
+        if (button == 4 || button == 8)
         {
             file_browser->go_back();
         }
@@ -2701,9 +2705,11 @@ on_window_button_press_event(GtkWidget* widget, GdkEventButton* event,
 }
 
 static bool
-on_main_window_keypress(MainWindow* main_window, GdkEventKey* event, xset_t known_set)
+on_main_window_keypress(MainWindow* main_window, GdkEvent* event, xset_t known_set)
 {
-    // ztd::logger::info("main_keypress {} {}", event->keyval, event->state);
+    const auto keymod = ptk_get_keymod(gdk_event_get_modifier_state(event));
+    const auto keyval = gdk_key_event_get_keyval(event);
+    // ztd::logger::debug("main_keypress {} {}", keyval, keymod);
 
     if (known_set)
     {
@@ -2711,29 +2717,21 @@ on_main_window_keypress(MainWindow* main_window, GdkEventKey* event, xset_t know
         return on_main_window_keypress_found_key(main_window, set);
     }
 
-    if (event->keyval == 0)
+    if (keyval == 0)
     {
         return false;
     }
 
     PtkFileBrowser* browser;
 
-    const u32 keymod = ptk_get_keymod(event->state);
-
-    if ((event->keyval == GDK_KEY_Home &&
-         (keymod == 0 || keymod == GdkModifierType::GDK_SHIFT_MASK)) ||
-        (event->keyval == GDK_KEY_End &&
-         (keymod == 0 || keymod == GdkModifierType::GDK_SHIFT_MASK)) ||
-        (event->keyval == GDK_KEY_Delete && keymod == 0) ||
-        (event->keyval == GDK_KEY_Tab && keymod == 0) ||
-        (keymod == 0 && (event->keyval == GDK_KEY_Return || event->keyval == GDK_KEY_KP_Enter)) ||
-        (event->keyval == GDK_KEY_Left &&
-         (keymod == 0 || keymod == GdkModifierType::GDK_SHIFT_MASK)) ||
-        (event->keyval == GDK_KEY_Right &&
-         (keymod == 0 || keymod == GdkModifierType::GDK_SHIFT_MASK)) ||
-        (event->keyval == GDK_KEY_BackSpace && keymod == 0) ||
-        (keymod == 0 && event->keyval != GDK_KEY_Escape &&
-         gdk_keyval_to_unicode(event->keyval))) // visible char
+    if ((keyval == GDK_KEY_Home && (keymod == 0 || keymod == GdkModifierType::GDK_SHIFT_MASK)) ||
+        (keyval == GDK_KEY_End && (keymod == 0 || keymod == GdkModifierType::GDK_SHIFT_MASK)) ||
+        (keyval == GDK_KEY_Delete && keymod == 0) || (keyval == GDK_KEY_Tab && keymod == 0) ||
+        (keymod == 0 && (keyval == GDK_KEY_Return || keyval == GDK_KEY_KP_Enter)) ||
+        (keyval == GDK_KEY_Left && (keymod == 0 || keymod == GdkModifierType::GDK_SHIFT_MASK)) ||
+        (keyval == GDK_KEY_Right && (keymod == 0 || keymod == GdkModifierType::GDK_SHIFT_MASK)) ||
+        (keyval == GDK_KEY_BackSpace && keymod == 0) ||
+        (keymod == 0 && keyval != GDK_KEY_Escape && gdk_keyval_to_unicode(keyval))) // visible char
     {
         browser = main_window->current_file_browser();
         if (browser && browser->path_bar() && gtk_widget_has_focus(GTK_WIDGET(browser->path_bar())))
@@ -2750,7 +2748,7 @@ on_main_window_keypress(MainWindow* main_window, GdkEventKey* event, xset_t know
         {
             // set has shared key
             set = xset_get(set->shared_key.value());
-            if (set->key == event->keyval && set->keymod == keymod)
+            if (set->key == keyval && set->keymod == keymod)
             {
                 // shared key match
                 if (ztd::startswith(set->name, "panel"))
@@ -2775,13 +2773,13 @@ on_main_window_keypress(MainWindow* main_window, GdkEventKey* event, xset_t know
                 continue;
             }
         }
-        if (set->key == event->keyval && set->keymod == keymod)
+        if (set->key == keyval && set->keymod == keymod)
         {
             return on_main_window_keypress_found_key(main_window, set);
         }
     }
 
-    if ((event->state & GdkModifierType::GDK_MOD1_MASK))
+    if ((keymod & GdkModifierType::GDK_MOD1_MASK))
     {
         rebuild_menus(main_window);
     }
@@ -2790,7 +2788,7 @@ on_main_window_keypress(MainWindow* main_window, GdkEventKey* event, xset_t know
 }
 
 bool
-main_window_keypress(MainWindow* main_window, GdkEventKey* event, xset_t known_set)
+main_window_keypress(MainWindow* main_window, GdkEvent* event, xset_t known_set)
 {
     return on_main_window_keypress(main_window, event, known_set);
 }

src/main-window.hxx
@@ -145,7 +145,7 @@ void main_window_open_network(MainWindow* main_window, const std::string_view ur
 void main_window_store_positions(MainWindow* main_window);
 
 void main_window_fullscreen_activate(MainWindow* main_window);
-bool main_window_keypress(MainWindow* main_window, GdkEventKey* event, xset_t known_set);
+bool main_window_keypress(MainWindow* main_window, GdkEvent* event, xset_t known_set);
 
 struct main_window_counts_data
 {

src/ptk/ptk-dir-tree-view.cxx
@@ -23,9 +23,12 @@
 #include <ztd/ztd.hxx>
 #include <ztd/ztd_logger.hxx>
 
+#include "compat/gtk4-porting.hxx"
+
 #include "ptk/ptk-dir-tree.hxx"
 #include "ptk/ptk-file-menu.hxx"
 #include "ptk/ptk-file-task.hxx"
+#include "ptk/ptk-keyboard.hxx"
 
 #include "ptk/ptk-dir-tree-view.hxx"
 
@@ -39,10 +42,10 @@ static void on_dir_tree_view_row_expanded(GtkTreeView* treeview, GtkTreeIter* it
 static void on_dir_tree_view_row_collapsed(GtkTreeView* treeview, GtkTreeIter* iter,
                                            GtkTreePath* path, void* user_data);
 
-static bool on_dir_tree_view_button_press(GtkWidget* view, GdkEventButton* evt,
+static bool on_dir_tree_view_button_press(GtkWidget* view, GdkEvent* event,
                                           PtkFileBrowser* browser);
 
-static bool on_dir_tree_view_key_press(GtkWidget* view, GdkEventKey* evt, PtkFileBrowser* browser);
+static bool on_dir_tree_view_key_press(GtkWidget* view, GdkEvent* event, PtkFileBrowser* browser);
 
 static bool sel_func(GtkTreeSelection* selection, GtkTreeModel* model, GtkTreePath* path,
                      bool path_currently_selected, void* data);
@@ -396,19 +399,25 @@ on_dir_tree_view_row_collapsed(GtkTreeView* treeview, GtkTreeIter* iter, GtkTree
 }
 
 static bool
-on_dir_tree_view_button_press(GtkWidget* view, GdkEventButton* event, PtkFileBrowser* file_browser)
+on_dir_tree_view_button_press(GtkWidget* view, GdkEvent* event, PtkFileBrowser* file_browser)
 {
     GtkTreePath* tree_path;
     GtkTreeViewColumn* tree_col;
     GtkTreeIter it;
 
-    if (event->type == GdkEventType::GDK_BUTTON_PRESS && (event->button == 1 || event->button == 3))
+    const auto button = gdk_button_event_get_button(event);
+    const auto type = gdk_event_get_event_type(event);
+
+    f64 x, y;
+    gdk_event_get_position(event, &x, &y);
+
+    if (type == GdkEventType::GDK_BUTTON_PRESS && (button == 1 || button == 3))
     {
         // middle click 2 handled in ptk-file-browser.c on_dir_tree_button_press
         GtkTreeModel* model = gtk_tree_view_get_model(GTK_TREE_VIEW(view));
         if (gtk_tree_view_get_path_at_pos(GTK_TREE_VIEW(view),
-                                          static_cast<i32>(event->x),
-                                          static_cast<i32>(event->y),
+                                          x,
+                                          y,
                                           &tree_path,
                                           &tree_col,
                                           nullptr,
@@ -418,7 +427,7 @@ on_dir_tree_view_button_press(GtkWidget* view, GdkEventButton* event, PtkFileBro
             {
                 gtk_tree_view_set_cursor(GTK_TREE_VIEW(view), tree_path, tree_col, false);
 
-                if (event->button == 3)
+                if (button == 3)
                 {
                     // right click
                     const std::filesystem::path dir_path =
@@ -446,12 +455,12 @@ on_dir_tree_view_button_press(GtkWidget* view, GdkEventButton* event, PtkFileBro
             gtk_tree_path_free(tree_path);
         }
     }
-    else if (event->type == GdkEventType::GDK_2BUTTON_PRESS && event->button == 1)
+    else if (type == GdkEventType::GDK_2BUTTON_PRESS && button == 1)
     {
-        // f64 click - expand/collapse
+        // double click - expand/collapse
         if (gtk_tree_view_get_path_at_pos(GTK_TREE_VIEW(view),
-                                          static_cast<i32>(event->x),
-                                          static_cast<i32>(event->y),
+                                          x,
+                                          y,
                                           &tree_path,
                                           nullptr,
                                           nullptr,
@@ -473,7 +482,7 @@ on_dir_tree_view_button_press(GtkWidget* view, GdkEventButton* event, PtkFileBro
 }
 
 static bool
-on_dir_tree_view_key_press(GtkWidget* view, GdkEventKey* event, PtkFileBrowser* file_browser)
+on_dir_tree_view_key_press(GtkWidget* view, GdkEvent* event, PtkFileBrowser* file_browser)
 {
     GtkTreeModel* model;
     GtkTreeIter iter;
@@ -484,14 +493,12 @@ on_dir_tree_view_key_press(GtkWidget* view, GdkEventKey* event, PtkFileBrowser* 
         return false;
     }
 
-    const i32 keymod =
-        (event->state & (GdkModifierType::GDK_SHIFT_MASK | GdkModifierType::GDK_CONTROL_MASK |
-                         GdkModifierType::GDK_MOD1_MASK | GdkModifierType::GDK_SUPER_MASK |
-                         GdkModifierType::GDK_HYPER_MASK | GdkModifierType::GDK_META_MASK));
+    const auto keymod = ptk_get_keymod(gdk_event_get_modifier_state(event));
+    const auto keyval = gdk_key_event_get_keyval(event);
 
     GtkTreePath* path = gtk_tree_model_get_path(model, &iter);
 
-    switch (event->keyval)
+    switch (keyval)
     {
         case GDK_KEY_Left:
         {
@@ -528,7 +535,7 @@ on_dir_tree_view_key_press(GtkWidget* view, GdkEventKey* event, PtkFileBrowser* 
         case GDK_KEY_F10:
         case GDK_KEY_Menu:
         {
-            if (event->keyval == GDK_KEY_F10 && keymod != GdkModifierType::GDK_SHIFT_MASK)
+            if (keyval == GDK_KEY_F10 && keymod != GdkModifierType::GDK_SHIFT_MASK)
             {
                 gtk_tree_path_free(path);
                 return false;

src/ptk/ptk-file-actions-rename.cxx
@@ -149,14 +149,15 @@ static void on_toggled(GtkMenuItem* item, MoveSet* mset);
 static const std::optional<std::filesystem::path> get_template_dir();
 
 static bool
-on_move_keypress(GtkWidget* widget, GdkEventKey* event, MoveSet* mset)
+on_move_keypress(GtkWidget* widget, GdkEvent* event, MoveSet* mset)
 {
     (void)widget;
-    const u32 keymod = ptk_get_keymod(event->state);
+    const auto keymod = ptk_get_keymod(gdk_event_get_modifier_state(event));
+    const auto keyval = gdk_key_event_get_keyval(event);
 
     if (keymod == 0)
     {
-        switch (event->keyval)
+        switch (keyval)
         {
             case GDK_KEY_Return:
             case GDK_KEY_KP_Enter:
@@ -173,14 +174,15 @@ on_move_keypress(GtkWidget* widget, GdkEventKey* event, MoveSet* mset)
 }
 
 static bool
-on_move_entry_keypress(GtkWidget* widget, GdkEventKey* event, MoveSet* mset)
+on_move_entry_keypress(GtkWidget* widget, GdkEvent* event, MoveSet* mset)
 {
     (void)widget;
-    const u32 keymod = ptk_get_keymod(event->state);
+    const auto keymod = ptk_get_keymod(gdk_event_get_modifier_state(event));
+    const auto keyval = gdk_key_event_get_keyval(event);
 
     if (keymod == 0)
     {
-        switch (event->keyval)
+        switch (keyval)
         {
             case GDK_KEY_Return:
             case GDK_KEY_KP_Enter:
@@ -1898,11 +1900,14 @@ copy_entry_to_clipboard(GtkWidget* widget, MoveSet* mset)
 }
 
 static bool
-on_label_button_press(GtkWidget* widget, GdkEventButton* event, MoveSet* mset)
+on_label_button_press(GtkWidget* widget, GdkEvent* event, MoveSet* mset)
 {
-    if (event->type == GdkEventType::GDK_BUTTON_PRESS)
+    const auto button = gdk_button_event_get_button(event);
+    const auto type = gdk_event_get_event_type(event);
+
+    if (type == GdkEventType::GDK_BUTTON_PRESS)
     {
-        if (event->button == 1 || event->button == 2)
+        if (button == 1 || button == 2)
         {
             GtkWidget* input = nullptr;
             if (widget == GTK_WIDGET(mset->label_name))
@@ -1929,7 +1934,7 @@ on_label_button_press(GtkWidget* widget, GdkEventButton* event, MoveSet* mset)
             {
                 gtk_label_select_region(mset->label_mime, 0, -1);
                 gtk_widget_grab_focus(GTK_WIDGET(mset->label_mime));
-                if (event->button == 2)
+                if (button == 2)
                 {
                     copy_entry_to_clipboard(widget, mset);
                 }
@@ -1955,14 +1960,14 @@ on_label_button_press(GtkWidget* widget, GdkEventButton* event, MoveSet* mset)
             {
                 select_input(input, mset);
                 gtk_widget_grab_focus(input);
-                if (event->button == 2)
+                if (button == 2)
                 {
                     copy_entry_to_clipboard(widget, mset);
                 }
             }
         }
     }
-    else if (event->type == GdkEventType::GDK_2BUTTON_PRESS)
+    else if (type == GdkEventType::GDK_2BUTTON_PRESS)
     {
         copy_entry_to_clipboard(widget, mset);
     }

commit adcc9c23ace84764733245e67e0bfe244ed4854f

fix filename exists dialog broken rename
broken in 7913d4d40af1e6b1f02bdf719f4644d838071f52

src/ptk/ptk-file-task.cxx
@@ -50,6 +50,7 @@
 #include "ptk/ptk-file-task-view.hxx"
 #include "ptk/ptk-file-task.hxx"
 #include "ptk/utils/multi-input.hxx"
+#include "ptk/utils/ptk-utils.hxx"
 
 static bool on_vfs_file_task_state_cb(const std::shared_ptr<vfs::file_task>& task,
                                       const vfs::file_task::state state, void* state_data,
@@ -1857,31 +1858,42 @@ static bool
 on_query_input_keypress(GtkWidget* widget, GdkEvent* event, ptk::file_task* ptask) noexcept
 {
     (void)ptask;
+    const auto keymod = ptk::utils::get_keymod(gdk_event_get_modifier_state(event));
     const auto keyval = gdk_key_event_get_keyval(event);
-    if (keyval == GDK_KEY_Return || keyval == GDK_KEY_KP_Enter)
+    if (keymod == 0)
     {
-        // User pressed enter in rename/overwrite dialog
-        const auto new_name = ptk::utils::multi_input_get_text(widget);
-        const char* old_name =
-            static_cast<const char*>(g_object_get_data(G_OBJECT(widget), "old_name"));
+        switch (keyval)
+        {
+            case GDK_KEY_Return:
+            case GDK_KEY_KP_Enter:
+            {
+                // User pressed enter in rename/overwrite dialog
+                const auto new_name = ptk::utils::multi_input_get_text(widget);
+                const char* old_name =
+                    static_cast<const char*>(g_object_get_data(G_OBJECT(widget), "old_name"));
 
 #if (GTK_MAJOR_VERSION == 4)
-        GtkWidget* parent = GTK_WIDGET(gtk_widget_get_root(GTK_WIDGET(widget)));
+                GtkWidget* parent = GTK_WIDGET(gtk_widget_get_root(GTK_WIDGET(widget)));
 #elif (GTK_MAJOR_VERSION == 3)
-        GtkWidget* parent = gtk_widget_get_toplevel(GTK_WIDGET(widget));
+                GtkWidget* parent = gtk_widget_get_toplevel(GTK_WIDGET(widget));
 #endif
 
-        if (new_name && old_name && new_name.value() != old_name)
-        {
-            gtk_dialog_response(GTK_DIALOG(parent),
-                                magic_enum::enum_integer(ptk::file_task::response::rename));
+                if (new_name && old_name && new_name.value() != old_name)
+                {
+                    gtk_dialog_response(GTK_DIALOG(parent),
+                                        magic_enum::enum_integer(ptk::file_task::response::rename));
+                }
+                else
+                {
+                    gtk_dialog_response(
+                        GTK_DIALOG(parent),
+                        magic_enum::enum_integer(ptk::file_task::response::auto_rename));
+                }
+                return true;
+            }
+            default:
+                break;
         }
-        else
-        {
-            gtk_dialog_response(GTK_DIALOG(parent),
-                                magic_enum::enum_integer(ptk::file_task::response::auto_rename));
-        }
-        return true;
     }
     return false;
 }
@@ -2412,6 +2424,7 @@ ptk::file_task::query_overwrite() noexcept
     // name input
     GtkScrolledWindow* scroll = GTK_SCROLLED_WINDOW(gtk_scrolled_window_new(nullptr, nullptr));
     GtkWidget* query_input = GTK_WIDGET(ptk::utils::multi_input_new(scroll, filename));
+    g_object_set_data(G_OBJECT(query_input), "old_name", ::utils::strdup(filename.data()));
     // clang-format off
     g_signal_connect(G_OBJECT(query_input), "key-press-event", G_CALLBACK(on_query_input_keypress), this);
     // clang-format on