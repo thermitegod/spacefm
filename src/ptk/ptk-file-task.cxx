thermitegod/spacefm

commit ba00d1b2081bd1931b9b571061eab0b247b93d3d

ptk::file_task class

src/ptk/ptk-file-task.hxx
@@ -43,7 +43,7 @@ struct file_task
         cont
     };
 
-    enum response
+    enum class response
     {
         overwrite = 1 << 0,
         overwrite_all = 1 << 1,
@@ -65,60 +65,83 @@ struct file_task
 
     std::shared_ptr<vfs::file_task> task{nullptr};
 
-    GtkWidget* progress_dlg{nullptr};
-    GtkButton* progress_btn_close{nullptr};
-    GtkButton* progress_btn_stop{nullptr};
-    GtkButton* progress_btn_pause{nullptr};
-    GtkWindow* parent_window{nullptr};
-    GtkWidget* task_view{nullptr};
-    GtkLabel* from{nullptr};
-    GtkLabel* to{nullptr};
-    GtkLabel* src_dir{nullptr};
-    GtkLabel* current{nullptr};
-    GtkProgressBar* progress_bar{nullptr};
-    GtkLabel* errors{nullptr};
-    GtkWidget* error_view{nullptr};
-    GtkScrolledWindow* scroll{nullptr};
-    GtkWidget* overwrite_combo{nullptr};
-    GtkWidget* error_combo{nullptr};
+    GtkWidget* progress_dlg_{nullptr};
+    GtkButton* progress_btn_close_{nullptr};
+    GtkButton* progress_btn_stop_{nullptr};
+    GtkButton* progress_btn_pause_{nullptr};
+    GtkWindow* parent_window_{nullptr};
+    GtkWidget* task_view_{nullptr};
+    GtkLabel* from_{nullptr};
+    GtkLabel* to_{nullptr};
+    GtkLabel* src_dir_{nullptr};
+    GtkLabel* current_{nullptr};
+    GtkProgressBar* progress_bar_{nullptr};
+    GtkLabel* errors_{nullptr};
+    GtkWidget* error_view_{nullptr};
+    GtkScrolledWindow* scroll_{nullptr};
+    GtkWidget* overwrite_combo_{nullptr};
+    GtkWidget* error_combo_{nullptr};
 
-    GtkTextBuffer* log_buf{nullptr};
-    GtkTextMark* log_end{nullptr};
-    bool log_appended{false};
-    i32 err_count{0};
-    ptk::file_task::ptask_error err_mode{ptk::file_task::ptask_error::first};
+    GtkTextBuffer* log_buf_{nullptr};
+    GtkTextMark* log_end_{nullptr};
+    bool log_appended_{false};
+    i32 err_count_{0};
+    ptk::file_task::ptask_error err_mode_{ptk::file_task::ptask_error::first};
 
-    bool complete{false};
-    bool aborted{false};
-    bool pause_change{false};
-    bool pause_change_view{true};
+    bool complete_{false};
+    bool aborted_{false};
+    bool pause_change_{false};
+    bool pause_change_view_{true};
 
     /* <private> */
-    u32 timeout{0};
-    bool restart_timeout{false};
-    u32 progress_timer{0};
-    u8 progress_count{0};
-    GFunc complete_notify{nullptr};
-    void* user_data{nullptr};
-    bool keep_dlg{false};
-    bool pop_detail{false};
+    u32 timeout_{0};
+    bool restart_timeout_{false};
+    u32 progress_timer_{0};
+    u8 progress_count_{0};
+    GFunc complete_notify_{nullptr};
+    void* user_data_{nullptr};
+    bool keep_dlg_{false};
+    bool pop_detail_{false};
 
-    GCond* query_cond{nullptr};
-    GCond* query_cond_last{nullptr};
-    char** query_new_dest{nullptr};
-    bool query_ret{false};
+    GCond* query_cond_{nullptr};
+    GCond* query_cond_last_{nullptr};
+    char** query_new_dest_{nullptr};
+    bool query_ret_{false};
 
-    std::string dsp_file_count{};
-    std::string dsp_size_tally{};
-    std::string dsp_elapsed{};
-    std::string dsp_curspeed{};
-    std::string dsp_curest{};
-    std::string dsp_avgspeed{};
-    std::string dsp_avgest{};
+    std::string dsp_file_count_{};
+    std::string dsp_size_tally_{};
+    std::string dsp_elapsed_{};
+    std::string dsp_curspeed_{};
+    std::string dsp_curest_{};
+    std::string dsp_avgspeed_{};
+    std::string dsp_avgest_{};
+
+    [[nodiscard]] bool is_completed() const noexcept;
+    [[nodiscard]] bool is_aborted() const noexcept;
+
+    void set_complete_notify(GFunc callback, void* user_data) noexcept;
+    void set_chmod(const std::array<u8, 12> chmod_actions) noexcept;
+    void set_chown(const uid_t uid, const gid_t gid) noexcept;
+    void set_recursive(const bool recursive) noexcept;
+    void run() noexcept;
+    bool cancel() noexcept;
+    void pause(const vfs::file_task::state state) noexcept;
+    void progress_open() noexcept;
 
     void lock() noexcept;
     bool trylock() noexcept;
     void unlock() noexcept;
+
+    void save_progress_dialog_size() const noexcept; // private
+
+    void query_overwrite() noexcept;
+
+    void update() noexcept;
+
+  private:
+    void set_button_states() noexcept;
+    void set_progress_icon() noexcept;
+    void progress_update() noexcept;
 };
 } // namespace ptk
 
@@ -136,19 +159,3 @@ ptk::file_task* ptk_file_exec_new(const std::string_view item_name, GtkWidget* p
 ptk::file_task* ptk_file_exec_new(const std::string_view item_name,
                                   const std::filesystem::path& dest_dir, GtkWidget* parent,
                                   GtkWidget* task_view);
-
-void ptk_file_task_set_complete_notify(ptk::file_task* ptask, GFunc callback, void* user_data);
-
-void ptk_file_task_set_chmod(ptk::file_task* ptask, std::array<u8, 12> chmod_actions);
-
-void ptk_file_task_set_chown(ptk::file_task* ptask, uid_t uid, gid_t gid);
-
-void ptk_file_task_set_recursive(ptk::file_task* ptask, bool recursive);
-
-void ptk_file_task_run(ptk::file_task* ptask);
-
-bool ptk_file_task_cancel(ptk::file_task* ptask);
-
-void ptk_file_task_pause(ptk::file_task* ptask, const vfs::file_task::state state);
-
-void ptk_file_task_progress_open(ptk::file_task* ptask);

src/main-window.cxx
@@ -251,7 +251,7 @@ MainWindow::open_terminal() const noexcept
     ptask->task->exec_command = terminal;
     ptask->task->exec_sync = false;
     ptask->task->exec_browser = file_browser;
-    ptk_file_task_run(ptask);
+    ptask->run();
 }
 
 static void

src/ptk/ptk-clipboard.cxx
@@ -432,9 +432,9 @@ ptk::clipboard::paste_files(GtkWindow* parent_win, const std::filesystem::path& 
                                                   GTK_WIDGET(task_view));
         if (callback && callback_win)
         {
-            ptk_file_task_set_complete_notify(ptask, callback, (void*)callback_win);
+            ptask->set_complete_notify(callback, (void*)callback_win);
         }
-        ptk_file_task_run(ptask);
+        ptask->run();
     }
     gtk_selection_data_free(sel_data);
 }
@@ -514,9 +514,9 @@ ptk::clipboard::paste_links(GtkWindow* parent_win, const std::filesystem::path& 
                                                   task_view ? GTK_WIDGET(task_view) : nullptr);
         if (callback && callback_win)
         {
-            ptk_file_task_set_complete_notify(ptask, callback, (void*)callback_win);
+            ptask->set_complete_notify(callback, (void*)callback_win);
         }
-        ptk_file_task_run(ptask);
+        ptask->run();
     }
     gtk_selection_data_free(sel_data);
 }
@@ -612,9 +612,9 @@ ptk::clipboard::paste_targets(GtkWindow* parent_win, const std::filesystem::path
                                                   GTK_WIDGET(task_view));
         if (callback && callback_win)
         {
-            ptk_file_task_set_complete_notify(ptask, callback, (void*)callback_win);
+            ptask->set_complete_notify(callback, (void*)callback_win);
         }
-        ptk_file_task_run(ptask);
+        ptask->run();
 
         if (missing_targets > 0)
         {

src/ptk/ptk-dir-tree-view.cxx
@@ -732,7 +732,7 @@ on_dir_tree_view_drag_data_received(GtkWidget* widget, GdkDragContext* drag_cont
                                                                   dest_dir,
                                                                   GTK_WINDOW(parent),
                                                                   file_browser->task_view());
-                        ptk_file_task_run(ptask);
+                        ptask->run();
                     }
                 }
                 gtk_drag_finish(drag_context, true, false, time);

src/ptk/ptk-file-action-misc.cxx
@@ -207,7 +207,7 @@ ptk::action::delete_files(GtkWindow* parent_win, const std::filesystem::path& cw
                                               file_list,
                                               parent_win ? GTK_WINDOW(parent_win) : nullptr,
                                               GTK_WIDGET(task_view));
-    ptk_file_task_run(ptask);
+    ptask->run();
 }
 
 void
@@ -244,5 +244,5 @@ ptk::action::trash_files(GtkWindow* parent_win, const std::filesystem::path& cwd
                                               file_list,
                                               parent_win ? GTK_WINDOW(parent_win) : nullptr,
                                               GTK_WIDGET(task_view));
-    ptk_file_task_run(ptask);
+    ptask->run();
 }

src/ptk/ptk-file-action-rename.cxx
@@ -3069,10 +3069,10 @@ ptk::action::rename_files(ptk::browser* file_browser, const char* file_dir,
                 {
                     auto_open->path = full_path;
                     auto_open->open_file = (response == GtkResponseType::GTK_RESPONSE_APPLY);
-                    ptask->complete_notify = auto_open->callback;
-                    ptask->user_data = auto_open;
+                    ptask->complete_notify_ = auto_open->callback;
+                    ptask->user_data_ = auto_open;
                 }
-                ptk_file_task_run(ptask);
+                ptask->run();
             }
             else if (create_new != ptk::action::rename_mode::rename && new_file)
             {
@@ -3129,10 +3129,10 @@ ptk::action::rename_files(ptk::browser* file_browser, const char* file_dir,
                 {
                     auto_open->path = full_path;
                     auto_open->open_file = (response == GtkResponseType::GTK_RESPONSE_APPLY);
-                    ptask->complete_notify = auto_open->callback;
-                    ptask->user_data = auto_open;
+                    ptask->complete_notify_ = auto_open->callback;
+                    ptask->user_data_ = auto_open;
                 }
-                ptk_file_task_run(ptask);
+                ptask->run();
             }
             else if (create_new != ptk::action::rename_mode::rename)
             {
@@ -3187,10 +3187,10 @@ ptk::action::rename_files(ptk::browser* file_browser, const char* file_dir,
                 {
                     auto_open->path = full_path;
                     auto_open->open_file = (response == GtkResponseType::GTK_RESPONSE_APPLY);
-                    ptask->complete_notify = auto_open->callback;
-                    ptask->user_data = auto_open;
+                    ptask->complete_notify_ = auto_open->callback;
+                    ptask->user_data_ = auto_open;
                 }
-                ptk_file_task_run(ptask);
+                ptask->run();
             }
             else if (copy || copy_target)
             {
@@ -3233,7 +3233,7 @@ ptk::action::rename_files(ptk::browser* file_browser, const char* file_dir,
                 ptask->task->exec_popup = false;
                 ptask->task->exec_show_output = false;
                 ptask->task->exec_show_error = true;
-                ptk_file_task_run(ptask);
+                ptask->run();
             }
             else if (link || link_target)
             {
@@ -3268,7 +3268,7 @@ ptk::action::rename_files(ptk::browser* file_browser, const char* file_dir,
                 ptask->task->exec_popup = false;
                 ptask->task->exec_show_output = false;
                 ptask->task->exec_show_error = true;
-                ptk_file_task_run(ptask);
+                ptask->run();
             }
             // need move?  (do move as task in case it takes a long time)
             else if (!std::filesystem::equivalent(old_path, path))
@@ -3288,7 +3288,7 @@ ptk::action::rename_files(ptk::browser* file_browser, const char* file_dir,
                 ptask->task->exec_popup = false;
                 ptask->task->exec_show_output = false;
                 ptask->task->exec_show_error = true;
-                ptk_file_task_run(ptask);
+                ptask->run();
             }
             else
             {

src/ptk/ptk-file-browser.cxx
@@ -2477,7 +2477,7 @@ on_folder_view_drag_data_received(GtkWidget* widget, GdkDragContext* drag_contex
                                                               dest_dir,
                                                               GTK_WINDOW(parent),
                                                               file_browser->task_view_);
-                    ptk_file_task_run(ptask);
+                    ptask->run();
                 }
                 gtk_drag_finish(drag_context, true, false, time);
                 return;
@@ -4158,7 +4158,7 @@ ptk::browser::copycmd(const std::span<const std::shared_ptr<vfs::file>> selected
                                                   dest_dir.value(),
                                                   GTK_WINDOW(parent_win),
                                                   this->task_view_);
-        ptk_file_task_run(ptask);
+        ptask->run();
     }
     else
     {
@@ -6199,7 +6199,7 @@ ptk::browser::on_permission(GtkMenuItem* item,
     ptask->task->exec_sync = true;
     ptask->task->exec_show_error = true;
     ptask->task->exec_show_output = false;
-    ptk_file_task_run(ptask);
+    ptask->run();
 }
 
 void

commit c79aa924e7c584fcad62040e3e5fb0f87a90be56

fix filename exists dialog cancel button
broken in ba00d1b2081bd1931b9b571061eab0b247b93d3d

src/ptk/ptk-file-task.cxx
@@ -35,7 +35,6 @@
 
 #include <magic_enum.hpp>
 
-#include <zmqpp/compatibility.hpp>
 #include <ztd/ztd.hxx>
 #include <ztd/ztd_logger.hxx>
 
@@ -1919,14 +1918,6 @@ on_multi_input_changed(GtkWidget* input_buf, GtkWidget* query_input) noexcept
 static void
 query_overwrite_response(GtkDialog* dlg, const i32 response, ptk::file_task* ptask) noexcept
 {
-    if (response == GtkResponseType::GTK_RESPONSE_DELETE_EVENT ||
-        response == GtkResponseType::GTK_RESPONSE_CANCEL)
-    {
-        // escape was pressed or window closed
-        ptask->task->abort = true;
-        return;
-    }
-
     switch (ptk::file_task::response(response))
     {
         case ptk::file_task::response::overwrite_all:
@@ -2008,8 +1999,15 @@ query_overwrite_response(GtkDialog* dlg, const i32 response, ptk::file_task* pta
             ptask->restart_timeout_ = false;
             break;
         }
-        default:
+        case ptk::file_task::response::close:
+        {
+            if (response == GtkResponseType::GTK_RESPONSE_CANCEL ||
+                response == GtkResponseType::GTK_RESPONSE_DELETE_EVENT)
+            { // escape was pressed or window closed
+                ptask->task->abort = true;
+            }
             break;
+        }
     }
 
     // save size
@@ -2032,8 +2030,8 @@ query_overwrite_response(GtkDialog* dlg, const i32 response, ptk::file_task* pta
     if (ptask->query_cond_)
     {
         ptask->lock();
-        ptask->query_ret_ = (response != GtkResponseType::GTK_RESPONSE_DELETE_EVENT) &&
-                            (response != GtkResponseType::GTK_RESPONSE_CANCEL);
+        ptask->query_ret_ = (response != GtkResponseType::GTK_RESPONSE_CANCEL) &&
+                            (response != GtkResponseType::GTK_RESPONSE_DELETE_EVENT);
         // g_cond_broadcast( ptask->query_cond );
         g_cond_signal(ptask->query_cond_);
         ptask->unlock();
@@ -2062,7 +2060,7 @@ on_query_button_press(GtkWidget* widget, ptk::file_task* ptask) noexcept
         return;
     }
 
-    ptk::file_task::response response;
+    ptk::file_task::response response = ptk::file_task::response::close;
     if (widget == rename_button)
     {
         response = ptk::file_task::response::rename;

src/ptk/ptk-file-task.hxx
@@ -45,14 +45,15 @@ struct file_task
 
     enum class response
     {
-        overwrite = 1 << 0,
-        overwrite_all = 1 << 1,
-        rename = 1 << 2,
-        skip = 1 << 3,
-        skip_all = 1 << 4,
-        auto_rename = 1 << 5,
-        auto_rename_all = 1 << 6,
-        pause = 1 << 7,
+        close, // close window
+        overwrite,
+        overwrite_all,
+        rename,
+        skip,
+        skip_all,
+        auto_rename,
+        auto_rename_all,
+        pause,
     };
 
     file_task() = delete;