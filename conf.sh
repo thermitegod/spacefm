395d676eb6994fb8de717d7da55770e96aa3f471
thermitegod <43489010+thermitegod@users.noreply.github.com>
2024-10-12 11:53:07 -0700

    conf.sh set default build flags

--- conf.sh
+++ conf.sh
@@ -3,7 +3,18 @@
 #CC=gcc CXX=g++ meson setup --buildtype=debug --prefix=$(pwd)/build ./build
 #CC=gcc CXX=g++ meson setup --buildtype=release --prefix=$(pwd)/build ./build
 
-CC=clang CXX=clang++ meson setup --buildtype=debug --prefix=$(pwd)/build ./build
+CC=clang CXX=clang++ meson setup \
+    -Ddeprecated=false \
+    -Dwith-system-cli11=true \
+    -Dwith-system-concurrencpp=true \
+    -Dwith-system-glaze=true \
+    -Dwith-system-magic-enum=true \
+    -Dwith-system-spdlog=true \
+    -Dwith-system-toml11=false \
+    -Dwith-system-ztd=true \
+    --buildtype=debug \
+    --prefix=$(pwd)/build ./build
+
 #CC=clang CXX=clang++ meson setup --buildtype=release --prefix=$(pwd)/build ./build
 
 #CC=clang CXX=clang++ meson setup --buildtype=debug --prefix=$(pwd)/build -Db_sanitize=undefined -Db_lundef=false ./build
