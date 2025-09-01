ba2b68241963938a26830330c05238a9bfde1072
thermitegod <43489010+thermitegod@users.noreply.github.com>
2025-09-01 11:59:19 -0700

    update clang-format.sh

--- clang-format.sh
+++ clang-format.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-find src/ -iname '*.cxx' -o -iname '*.hxx' | xargs --max-args=$(nproc) clang-format -i
-find dialog/ -iname '*.cxx' -o -iname '*.hxx' | xargs --max-args=$(nproc) clang-format -i
-find tools/ -iname '*.cxx' -o -iname '*.hxx' | xargs --max-args=$(nproc) clang-format -i
-find tests/ -iname '*.cxx' -o -iname '*.hxx' | xargs --max-args=$(nproc) clang-format -i
+find \
+    ./src \
+    ./dialog \
+    ./tools \
+    ./tests \
+    -iname '*.cxx' -o -iname '*.hxx' | \
+    clang-format -i --files=/dev/stdin
