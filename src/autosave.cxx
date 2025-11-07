8bfc15ca637ee9ab841d844dcb9838349f76e7a3
thermitegod <43489010+thermitegod@users.noreply.github.com>
2025-11-06 22:42:14 -0700

    autosave convert to std::thread

--- src/autosave.cxx
+++ src/autosave.cxx
@@ -13,83 +13,153 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include <memory>
+#include <chrono>
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+
+#include <pthread.h>
 
 #include <ztd/ztd.hxx>
 
 #include "autosave.hxx"
-#include "concurrency.hxx"
 #include "logger.hxx"
 
-struct requests_data
+class autosave_backend
 {
-    std::mutex mutex;
-    i32 total{0};
-    bool pending{false};
+  public:
+    void run() noexcept;
+    void run_once() noexcept;
+    void stop() noexcept;
+
+    void add() noexcept;
+    void cancel() noexcept;
+
+    [[nodiscard]] bool
+    is_stopped() const noexcept
+    {
+        return this->stopped_;
+    }
+
+    void
+    set_autosave_func(const std::function<void()>& autosave_func) noexcept
+    {
+        this->autosave_func_ = autosave_func;
+    }
+
+  private:
+    std::mutex mutex_;
+    std::condition_variable cv_;
+    bool stopped_ = false;
+    std::chrono::minutes thread_sleep_ = std::chrono::minutes(5);
+
+    std::function<void()> autosave_func_;
+    i32 total_ = 0;
+    bool pending_ = false;
 };
 
-const auto requests = std::make_unique<requests_data>();
+void
+autosave_backend::run() noexcept
+{
+    while (!this->is_stopped())
+    {
+        this->run_once();
+    }
+}
+
+void
+autosave_backend::run_once() noexcept
+{
+    {
+        std::unique_lock<std::mutex> lock(this->mutex_);
+        this->cv_.wait_for(lock, this->thread_sleep_, [this] { return this->is_stopped(); });
+
+        if (this->is_stopped())
+        {
+            return;
+        }
+    }
+
+    logger::trace<logger::autosave>("checking for pending autosave requests");
+    if (this->pending_)
+    {
+        {
+            const std::scoped_lock<std::mutex> lock(this->mutex_);
+
+            logger::trace<logger::autosave>(
+                "found autosave requests, saving settings, total request for this period {}",
+                this->total_);
+
+            this->autosave_func_();
+
+            this->total_ = 0;
+            this->pending_ = false;
+        }
+    }
+}
+
+void
+autosave_backend::stop() noexcept
+{
+    {
+        std::lock_guard<std::mutex> lock(this->mutex_);
+        this->stopped_ = true;
+    }
+    this->cv_.notify_all();
+}
+
+void
+autosave_backend::add() noexcept
+{
+    const std::scoped_lock<std::mutex> lock(this->mutex_);
+
+    this->total_ += 1;
+    logger::trace<logger::autosave>("adding request, total {}", this->total_);
+    this->pending_ = true;
+}
+
+void
+autosave_backend::cancel() noexcept
+{
+    const std::scoped_lock<std::mutex> lock(this->mutex_);
+
+    logger::trace<logger::autosave>("canceling '{}' requests", this->total_);
+    this->total_ = 0;
+    this->pending_ = false;
+}
+
+namespace
+{
+autosave_backend autosave_;
+std::jthread autosave_thread_;
+} // namespace
 
 void
 autosave::request_add() noexcept
 {
-    const std::scoped_lock<std::mutex> lock(requests->mutex);
-
-    requests->total += 1;
-    logger::trace<logger::autosave>("adding request, total {}", requests->total);
-    requests->pending = true;
+    autosave_.add();
 }
 
 void
 autosave::request_cancel() noexcept
 {
-    const std::scoped_lock<std::mutex> lock(requests->mutex);
-
-    logger::trace<logger::autosave>("canceling request");
-    requests->total = 0;
-    requests->pending = false;
+    autosave_.cancel();
 }
 
-namespace autosave::detail
-{
-static concurrencpp::timer timer;
-} // namespace autosave::detail
-
 void
-autosave::create(const std::function<void()> autosave_func) noexcept
+autosave::create(const std::function<void()>& autosave_func) noexcept
 {
+    autosave_.set_autosave_func(autosave_func);
+
     logger::trace<logger::autosave>("starting autosave thread");
 
-    using namespace std::chrono_literals;
-
-    // clang-format off
-    autosave::detail::timer = global::runtime.timer_queue()->make_timer(
-        300000ms, // 5 Minutes
-        300000ms, // 5 Minutes
-        global::runtime.thread_executor(),
-        [&autosave_func]
-        {
-            logger::trace<logger::autosave>("checking for pending autosave requests");
-            if (requests->pending)
-            {
-                {
-                    const std::scoped_lock<std::mutex> lock(requests->mutex);
-
-                    logger::trace<logger::autosave>(
-                        "found autosave requests, saving settings, total request for this period {}",
-                        requests->total);
-                    requests->total = 0;
-                    requests->pending = false;
-                }
-
-                autosave_func();
-            }
-        });
-    // clang-format on
+    autosave_thread_ = std::jthread([&]() { autosave_.run(); });
+    pthread_setname_np(autosave_thread_.native_handle(), "autosave");
 }
 
 void
 autosave::close() noexcept
 {
-    autosave::detail::timer.cancel();
+    autosave_.stop();
+    autosave_thread_.join();
 }
