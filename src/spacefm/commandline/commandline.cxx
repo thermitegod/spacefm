0fbff9f5b82a586709bfdab7df5ca286b5b23f31
thermitegod <43489010+thermitegod@users.noreply.github.com>
2026-08-15 10:24:58 -0700

    meson.build cleanup

--- src/spacefm/commandline/commandline.cxx
+++ src/spacefm/commandline/commandline.cxx
@@ -86,7 +86,6 @@ run_commandline(const std::shared_ptr<opts_data>& opt) noexcept
         std::println("PACKAGE_ONLINE_DOCS   = {}", PACKAGE_ONLINE_DOCS);
         std::println("PACKAGE_IMAGES        = {}", PACKAGE_IMAGES);
         std::println("PACKAGE_IMAGES_LOCAL  = {}", PACKAGE_IMAGES_LOCAL);
-        std::println("DEV_SCRIPTS_PATH      = {}", DEV_SCRIPTS_PATH);
         std::exit(EXIT_SUCCESS);
     }
 #endif
